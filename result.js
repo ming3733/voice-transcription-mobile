c138d0cb01f573c3716de1778518f7653add5661
Jemmy zhou <32428787+ming3733@users.noreply.github.com>
2026-06-17 14:14:59 +0800

    Add result.js

---
+++ result.js
@@ -0,0 +1,7 @@
+document.getElementById("rerecordButton").addEventListener("click", () => {
+  window.location.href = "./recording.html";
+});
+
+document.getElementById("confirmButton").addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
