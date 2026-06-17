d9739544f1c7eaa5d7e2c8e718037ca621e2fe31
Jemmy zhou <32428787+ming3733@users.noreply.github.com>
2026-06-17 14:15:00 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById("startButton");
+const copyBlock = document.querySelector(".copy-block");
+
+startButton.addEventListener("click", () => {
+  window.location.href = "./recording.html";
+});
